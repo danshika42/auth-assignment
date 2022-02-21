6a94a76efa461e90573c7d06cfa3ed6abed30351
danshika42 <83402199+danshika42@users.noreply.github.com>
2022-02-21 21:44:09 +0530

    --updated fb login

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React,{useState} from 'react';
 import { auth } from './firebase';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { FacebookAuthProvider } from "firebase/auth";
 
 function App() {
   const [name,setName]=useState('');
   const [email,setEmail]=useState('');
   const [photoSrc,setPhotoSrc]=useState('');
 
-  const signInWithFirebase=()=>{
+  const signInWithGoogle=()=>{
     const google_provider=new GoogleAuthProvider();
     signInWithPopup(auth,google_provider)
     .then((res)=>{
@@ -19,12 +20,31 @@ function App() {
       console.log(error.message)
     }); 
   }
+
+  const signInWithFacebook=()=>{
+      const facebook_provider = new FacebookAuthProvider();
+      signInWithPopup(auth, facebook_provider)
+        .then((result) => {
+          const user = result.user;
+          const credential = FacebookAuthProvider.credentialFromResult(result);
+          const accessToken = credential.accessToken;
+          console.log(result)
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          const email = error.email;
+          const credential = FacebookAuthProvider.credentialFromError(error);
+          console.log(error);
+        });
+  }
   return (
     <div className="App">
-      <button onClick={signInWithFirebase}>Sign with google</button>
+      <button onClick={signInWithGoogle}>Sign with google</button>
       <p>{name}</p>
       <p>{email}</p>
       <img src={photoSrc} />
+      <button onClick={signInWithFacebook}>Sign with facebook</button>
     </div>
   );
 }
